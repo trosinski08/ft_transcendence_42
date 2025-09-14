c362632c72210f42e6cabed7d013d12f8b670150
trosinski08 <trosinski08@gmail.com>
2025-09-14 18:34:39 +0200

    build: add TypeScript + Webpack toolchain

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'main.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    host: '0.0.0.0',
+    port: 3000,
+    hot: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader','css-loader'] }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })
+  ]
+};
