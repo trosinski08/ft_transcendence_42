16bb01248c45abf7a9a8bac0ae1623cec38bfc01
trosinski08 <trosinski08@gmail.com>
2025-09-21 20:55:57 +0200

    build(webpack): extract CSS in production with MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'main.ts'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    clean: true,
-    publicPath: '/'
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    host: '0.0.0.0',
-    port: 3000,
-    hot: true
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: ['style-loader','css-loader','postcss-loader'] }
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+  return {
+    mode: isProd ? 'production' : 'development',
+    entry: path.resolve(__dirname, 'src', 'main.ts'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      clean: true,
+      publicPath: '/'
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'dist'),
+      host: '0.0.0.0',
+      port: 3000,
+      hot: true
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
+        { test: /\.css$/, use: [isProd ? MiniCssExtractPlugin.loader : 'style-loader','css-loader','postcss-loader'] }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
+      ...(isProd ? [new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })] : [])
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })
-  ]
+  };
 };
