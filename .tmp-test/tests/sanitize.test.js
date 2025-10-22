3a09536bfb9faaa44104e92aa1d8eb79337b1397
trosinski08 <trosinski08@gmail.com>
2025-10-22 15:46:15 +0200

    chore(integration): curate deliverable (remove tests/docs/mock-api) and add ELK stack configs (compose+logstash) for DevOps; keep only runtime-relevant files

--- .tmp-test/tests/sanitize.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.run = run;
-const sanitize_1 = require("../src/utils/sanitize");
-function run(assert) {
-    const s = (0, sanitize_1.sanitize)('  A   B   ');
-    assert(s === 'A B', 'sanitize collapses whitespace and trims');
-    assert((0, sanitize_1.sanitize)('') === '', 'sanitize handles empty input');
-}
