a4903db3cf25f2a124597d3e511ba8671396ea43
Yacoub Cherouali <ycheroua@3-G-5.42heilbronn.de>
2025-11-02 16:23:28 +0100

    adding grafana to project

--- nginx/ssl/generate-cert.sh
+++ nginx/ssl/generate-cert.sh
@@ -3,3 +3,6 @@ set -e
 openssl req -x509 -nodes -days 365 \
   -subj "/C=PL/ST=Local/L=Local/O=Dev/OU=Dev/CN=localhost" \
   -newkey rsa:2048 -keyout server.key -out server.crt
+
+# Generate a self-signed cert and key for localhost
+echo "Generated server.key and server.crt in $(pwd)"
